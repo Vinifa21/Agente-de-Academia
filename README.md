3decb90bfd46766234e567e5f56e7ea33214472e
Vinifa21 <vini@Acer-Vinicius.localdomain>
2025-10-23 13:41:06 -0300

    Melhorando formatação da saída

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Trabalho feito para a Disiciplina de Inteligência Artificial. Criando um Agente com Prolog, que realiza busca desinformada
+Trabalho feito para a Disiciplina de Inteligência Artificial. Criando um Agente com Prolog, que realiza busca desinformada.
+
+Descrição: Para fazer um treino completo, o agente precisa realizar um exercício de cada grupo muscular. Para cada exercício, ele deve buscar os pesos que serão usados e depois retorná-los. As máquinas dos exercícios podem estar ou não ocupadas e, caso todas as máquinas de um determinado grupo muscular estejam ocupadas e o agente não consiga treinar um grupo muscular, o treino será considerado “impossível”. 
+
--- agente.pl
+++ agente.pl
@@ -1,22 +1,21 @@
-%VERSÃO FINAL 100% CORRETA ATUALIZADA 2025
-% ESTADOS INICIAL E FINAL
+:- encoding(utf8). % permite acentuação
+
 estado_final(estado(_, _, peito, costas, braco, perna)).
 
 % DEFINIÇÃO DO CENÁRIO
 peso(coord(5, 2)).
-invalidos([coord(9,2) , coord(1,2), coord(2,2)]).
-maquinas_peito([coord(1,1) , coord(1,2) , coord(1,3)]).
-maquinas_braco([coord(9,1) , coord(9,2) , coord(9,3)]).
-maquinas_costas([coord(4,6) , coord(5,6) , coord(6,6)]).
-
+invalidos([coord(9,2), coord(1,2), coord(2,2)]).
+maquinas_peito([coord(1,1), coord(1,2), coord(1,3)]).
+maquinas_braco([coord(9,1), coord(9,2), coord(9,3)]).
+maquinas_costas([coord(4,6), coord(5,6), coord(6,6)]).
 
 % VALIDAÇÃO DE COORDENADA
 eh_valido(coord(X, Y)) :- 
     invalidos(L),
-    \+pertence(coord(X,Y), L),
+    \+ pertence(coord(X,Y), L),
     ((X =< 9, Y =< 3, X >= 1, Y >= 1);
-    (X =:= 5, Y =:= 0);
-    (X >= 4, X =< 6, Y >= 4, Y =< 6)).
+     (X =:= 5, Y =:= 0);
+     (X >= 4, X =< 6, Y >= 4, Y =< 6)).
 
 % AÇÕES DE MOVIMENTO
 acao(anda_cima, estado(coord(X, Y), A, B, C, D, E), estado(coord(X, NewY), A, B, C, D, E)) :-
@@ -39,12 +38,13 @@ acao(anda_dir, estado(coord(X, Y), A, B, C, D, E), estado(coord(NewX, Y), A, B, 
     eh_valido(coord(NewX, Y)),
     (((NewX =:= 4 ; NewX =:= 7) , Y =:= 2); (NewX =\= 4 , NewX =\= 7)).
 
-% AÇÃO DE INTERAÇÃO
+
+% AÇÕES DE INTERAÇÃO
 acao(pega_peso, estado(Coord_atual, sem_peso, M, C, D, E), estado(Coord_atual, com_peso, M, C, D, E)) :-
     peso(Coord_atual).
 
 acao(treina_peito, estado(Coord_atual, com_peso, sem_peito, C, D, E), estado(Coord_atual, sem_peso, peito, C, D, E)) :- 
-    maquinas_peito(Lista), pertence(Coord_atual,Lista).
+    maquinas_peito(Lista), pertence(Coord_atual,Lista). 
 
 acao(treina_costas, estado(Coord_atual, com_peso, C, sem_costas, D, E), estado(Coord_atual, sem_peso, C, costas, D, E)) :- 
     maquinas_costas(Lista), pertence(Coord_atual,Lista).
@@ -54,7 +54,8 @@ acao(treina_braco, estado(Coord_atual, com_peso, D, C, sem_braco, E), estado(Coo
 
 acao(treina_perna, estado(A, B, peito, costas, braco, sem_perna), estado(A, B, peito, costas, braco, perna)).
 
-% PRIMITIVAS
+
+% PRIMITIVAS DE LISTA
 pertence(Elem, [Elem|_]).
 pertence(Elem, [_|Cauda]) :- pertence(Elem, Cauda).
 
@@ -65,44 +66,72 @@ concatena([Cab|Cauda], L2, [Cab|Resultado]) :-
 reverter([], []).
 reverter([H|T], R) :-
     reverter(T, RT),
-    append(RT, [H], R). 
+    append(RT, [H], R).
 
-% BUSCA EM LARGURA
-% Ponto de entrada
+
+%BUSCA EM LARGURA
+%Ponto de entrada
 solucao_bl(EstadoInicial, CaminhoCorreto) :- 
-    bl([[EstadoInicial]], CaminhoInvertido),
+    bl([[acao(inicio, EstadoInicial)]], CaminhoInvertido),
     reverter(CaminhoInvertido, CaminhoCorreto).
 
 % Caso base: encontrou o estado final
-bl([[Estado | Caminho] | _], [Estado | Caminho]) :- 
+bl([[acao(Acao, Estado) | Caminho] | _], [acao(Acao, Estado) | Caminho]) :- 
     estado_final(Estado).
 
-% Caso recursivo: expande fronteira
+% Caso recursivo: expande a fronteira
 bl([Primeiro | Outros], Solucao) :-
     estende(Primeiro, Sucessores),
     concatena(Outros, Sucessores, NovaFronteira),
     bl(NovaFronteira, Solucao).
 
-% Geração de sucessores
-estende([Estado | Caminho], Lista_sucessores) :-
+% Geração de sucessores (mantendo ações corretamente)
+estende([acao(AcaoAtual, EstadoAtual) | Caminho], ListaSucessores) :-
     findall(
-        [Sucessor, Estado | Caminho],
+        NovoCaminho,
         (
-            acao(_, Estado, Sucessor),
-            not(pertence(Sucessor, [Estado | Caminho]))
+            acao(AcaoNova, EstadoAtual, Sucessor),
+            \+ (member(acao(_, S), [acao(AcaoAtual, EstadoAtual) | Caminho]), S == Sucessor),
+            copy_term([acao(AcaoNova, Sucessor), acao(AcaoAtual, EstadoAtual) | Caminho], NovoCaminho)
         ),
-        Lista_sucessores
+        ListaSucessores
     ).
 
-% IMPRIMIR CAMINHO
+
+
+%Predicado para identificar ações de movimento
+movimento(anda_cima).
+movimento(anda_baixo).
+movimento(anda_esq).
+movimento(anda_dir).
+
+%DESCRIÇÃO DAS AÇÕES
+descreve_acao(anda_cima,   "Nurilo Maldi andou para cima").
+descreve_acao(anda_baixo,  "Nurilo Maldi andou para baixo").
+descreve_acao(anda_esq,    "Nurilo Maldi andou para a esquerda").
+descreve_acao(anda_dir,    "Nurilo Maldi andou para a direita").
+descreve_acao(pega_peso,   "Nurilo Maldi pegou o peso").
+descreve_acao(treina_peito, "Nurilo Maldi treinou peito").
+descreve_acao(treina_costas,"Nurilo Maldi treinou costas").
+descreve_acao(treina_braco, "Nurilo Maldi treinou braço").
+descreve_acao(treina_perna, "Como Nurilo Maldi andou muito, considera-se que ele treinou perna").
+descreve_acao(inicio,      "Início do treino").
+
+%IMPRIMIR CAMINHO
 imprimir_caminho([]).
-imprimir_caminho([Estado | Resto]) :-
-    writeln(Estado),
+imprimir_caminho([acao(Acao, Estado) | Resto]) :-
+    ( Estado = estado(coord(X, Y), _, _, _, _, _) -> true ; X = _, Y = _ ),
+    descreve_acao(Acao, Texto),
+    (   movimento(Acao)
+    ->
+        format("~w e agora está em (~w,~w).~n", [Texto, X, Y])
+    ;
+        format("~w.~n", [Texto])
+    ),
     imprimir_caminho(Resto).
 
-%exemplo de entrada no terminal:
-% solucao_bl(estado(coord(5,0),sem_peso,peito, sem_costas, braco, perna), X), imprimir_caminho(X).
-% solucao_bl(estado(coord(5,0),sem_peso, peito, sem_costas, sem_braco, perna), X), imprimir_caminho(X).
 
+% EXEMPLO DE CONSULTA
+% ?- solucao_bl(estado(coord(5,0), sem_peso, peito, sem_costas, braco, sem_perna), Caminho), imprimir_caminho(Caminho).
 %CONSULTA MAIS TRABALHOSA POSSIVEL:
 %  solucao_bl(estado(coord(5,0),sem_peso, sem_peito, costas, sem_braco, sem_perna), X), imprimir_caminho(X).
